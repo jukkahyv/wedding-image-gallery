31b4885b00a13dcfb4e11c272d3fd7c5b680bd6e
Riipah <riipah@vocadb.net>
2020-05-21 18:30:20 +0300

    Add project files.

---
+++ Server/Controllers/ImageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeddingImageGallery.Shared;
+
+namespace WeddingImageGallery.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+
+        private string Folder { get; } = @"C:\Users\Jukka\Pictures\CD";
+
+        private async Task<string> GetViewUrl(string file)
+        {
+
+        }
+
+        public async Task<IEnumerable<ImageProperties>> GetImages()
+        {
+            var images = Directory.GetFiles(Folder, "*.jpg;*.png");
+
+        }
+    }
+}
---
+++ Shared/ImageProperties.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeddingImageGallery.Shared
+{
+    public class ImageProperties
+    {
+        public string ViewUrl { get; set; }
+    }
+}
