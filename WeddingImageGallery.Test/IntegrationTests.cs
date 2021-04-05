8a19b3c8ea9b7632e8dcb1677d87149d3a190f6b
Riipah <riipah@vocadb.net>
2021-04-05 13:21:36 +0300

    add basic test

--- WeddingImageGallery.Test/IntegrationTests.cs
+++ WeddingImageGallery.Test/IntegrationTests.cs
@@ -1,12 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using WeddingImageGallery.Server;
+using Xunit;
 
 namespace WeddingImageGallery.Test {
 
-	public class IntegrationTests {
+	public class IntegrationTests : IClassFixture<WebApplicationFactory<Startup>> {
 
+		private readonly WebApplicationFactory<Startup> factory;
 
+		public IntegrationTests(WebApplicationFactory<Startup> factory) {
+			this.factory = factory;
+		}
+
+		[Fact]
+		public async Task GetGalleries() {
+
+			var client = factory.CreateClient();
+
+			var response = await client.GetAsync("/api/galleries");
+
+			response.EnsureSuccessStatusCode();
+
+		}
 
 	}
 
